03faad8330f763d4bae72e22662fa53954048eca
oopanpan <oopanpan@gmail.com>
2021-02-08 20:30:23 -0800

    model validations added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presense: true
+    validates :content, length: {minimum: 100}
+    vallidates :category, inclusiion: {in: %w(Fiction Non-Fiction)}
 end
